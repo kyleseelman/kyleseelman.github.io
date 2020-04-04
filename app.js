c295d63a63e349a089b095673231f18f998308a9
Kyle Seelman <kgseelman@gmail.com>
2020-04-03 22:37:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#ffedd1"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#ffedd1"
       },
       "polygon": {
         "nb_sides": 5
